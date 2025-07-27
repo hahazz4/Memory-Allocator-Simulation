2de98143adad9f670eca6a39bfb4f5d2a59ae11d
z.asim <Z_zee09@hotmail.com>
2025-07-27 00:24:34 -0400

    almosttttt done, finished block split, need to merge small free blocks!

--- allocate.cpp
+++ allocate.cpp
@@ -17,13 +17,27 @@ void *simMalloc(size_t size){
     while (curr != nullptr){
         //Check if the block is free for allocation, and if the current size of the block is big enough
         if (curr -> size >= size && curr -> status == true){
-            if 
-            
+            //current block size decreases to the size needed
+            if (curr -> size > size + sizeof(Block)){
+                //Need to move to the data portion of the block
+                char* dataBeg = (char*)(curr + 1);
+                char* newBlockAddr = dataBeg + size; //Go forward to the new block by the amount of bytes requested
+                Block* newBlock = (Block*) newBlockAddr; //Cast it into a new Block
+
+                newBlock -> size = curr -> size - size - sizeof(Block); //curr->size is the size of the current Block before seperation, size is the requested amount of data, and sizeof(Block) is the new block's header size
+                newBlock -> status = true; //Updating the new block to free (ready to be allocated)
+                newBlock -> next = curr -> next; //Updating the new block to point towards the next block of the memory pool
+
+                curr -> size = size; //Shrinks original block size to what was requested since new block is now what remains of the unused portion
+                curr -> next = newBlock; //Pointing the original block's next pointer to link towards the new block
+            }
+
             curr -> status = false;
             return (void*)(curr + 1); //returning the pointer of an unknown data type back to user, and moving to the next memory after Block header
         }
         curr = curr -> next;
     }
+    
     //Once the exit condition of the while loop is met that means no Block found, return a null pointer
     return nullptr;
 }
